9a113d9a2092967d462ffb79d0bf809b7dba928c
Sadaqat Ali <46186868+SadaqatDahani786@users.noreply.github.com>
2024-08-14 21:14:12 +0500

    Added header and footer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,25 @@
+/*
+ ** **
+ ** ** ** Feather Icons
+ ** **
+ */
+feather.replace();
 
+/*
+ ** **
+ ** ** ** Utils Funcs
+ ** **
+ */
+const select = (query) => document.querySelector(query);
+const selectAll = (query) => document.querySelectorAll(query);
+
+/*
+ ** **
+ ** ** ** Close header alert
+ ** **
+ */
+select(".header .alert button").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  select(".header .alert").style.display = "none";
+});
